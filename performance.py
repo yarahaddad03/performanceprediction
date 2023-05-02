74f0ae4278527d34dab766d4494c3ee75ceeb8e7
yarahaddad03 <114431403+yarahaddad03@users.noreply.github.com>
2023-05-02 18:05:09 +0300

    Update performance.py

--- performance.py
+++ performance.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 def load_model():
-    with open('C:/Users/lenovo/Desktop/Capstone/saved_steps.pkl', 'rb') as file:
+    with open('saved_steps.pkl', 'rb') as file:
         data=pickle.load(file)
     return data
 
